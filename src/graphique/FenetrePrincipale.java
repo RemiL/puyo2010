1a15bbb5cff1a33f61b51e0d7ce87982aff0d35a
remi.lacroix <remi.lacroix@984e1f29-0a54-0ee0-b2e0-9a448cd44014>
2010-05-15 19:20:13 +0000

    - mise en place des bases pour la partie graphique :     * fenêtre     * zone de jeu - possibilité de tracer des cercles ...

--- src/graphique/FenetrePrincipale.java
+++ src/graphique/FenetrePrincipale.java
@@ -1,14 +1,24 @@
 package graphique;
 
+import java.awt.BorderLayout;
+import java.awt.Container;
+
 import javax.swing.JFrame;
 
 public class FenetrePrincipale extends JFrame
 {
 	private static final long serialVersionUID = 4690329553231316904L;
 	
-	public FenetrePrincipale(String title, int width, int height)
+	public FenetrePrincipale(String title, int width, int height, ZoneDeJeu zdj)
 	{
 		super(title);
-		setSize(width, height);
+		this.setSize(width, height);
+		
+		Container pane = this.getContentPane();
+		pane.setLayout(new BorderLayout());
+		pane.add(zdj, BorderLayout.CENTER);
+		
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setVisible(true);
 	}
 }
--- src/graphique/ZoneDeJeu.java
+++ src/graphique/ZoneDeJeu.java
@@ -1,46 +1,85 @@
 package graphique;
 
+import java.awt.Point;
+
+import javax.media.opengl.DebugGL;
+import javax.media.opengl.GL;
 import javax.media.opengl.GLAutoDrawable;
 import javax.media.opengl.GLCanvas;
 import javax.media.opengl.GLCapabilities;
 import javax.media.opengl.GLEventListener;
+import javax.media.opengl.glu.GLU;
+
+import com.sun.opengl.util.FPSAnimator;
 
 public class ZoneDeJeu extends GLCanvas implements GLEventListener
 {
 	private static final long serialVersionUID = 2421808737959876690L;
+	/** The GL unit (helper class). */
+    private GLU glu;
+    private FPSAnimator animator;
 	
 	public ZoneDeJeu(GLCapabilities capabilities, int width, int height)
 	{
 		super(capabilities);
-		setSize(width, height);
-		addGLEventListener(this);
+		this.setSize(width, height);
+		
+		this.addGLEventListener(this);
 	}
-
+	
 	@Override
-	public void display(GLAutoDrawable arg0)
+	public void init(GLAutoDrawable drawable)
+	{
+		glu = new GLU();
+		
+		drawable.setGL(new DebugGL(drawable.getGL()));
+        final GL gl = drawable.getGL();
+        gl.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+        animator = new FPSAnimator(this, 60);
+        animator.start();
+	}
+	
+	@Override
+	public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height)
+	{
+		final GL gl = drawable.getGL();
+        gl.glViewport(0, 0, width, height);
+        gl.glMatrixMode(GL.GL_PROJECTION);
+        gl.glLoadIdentity();
+        glu.gluOrtho2D(0.0, width, 0.0, height); 
+	}
+	
+	@Override
+	public void display(GLAutoDrawable drawable)
+	{
+		final GL gl = drawable.getGL();
+		
+		gl.glClear(GL.GL_COLOR_BUFFER_BIT);
+		
+		dessinerCercle(gl, new Point(getWidth()/2, getHeight()/2), 100);
+		
+		gl.glFlush();
+	}
+	
+	@Override
+	public void displayChanged(GLAutoDrawable drawable, boolean modeChanged, boolean deviceChanged)
 	{
 		// TODO Auto-generated method stub
 
 	}
-
-	@Override
-	public void displayChanged(GLAutoDrawable arg0, boolean arg1, boolean arg2)
+	
+	public void dessinerCercle(GL gl, Point c, double rayon)
 	{
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void init(GLAutoDrawable arg0)
-	{
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void reshape(GLAutoDrawable arg0, int arg1, int arg2, int arg3, int arg4)
-	{
-		// TODO Auto-generated method stub
-
+		double x, y;
+		
+		gl.glBegin (GL.GL_POLYGON);       
+			for (double theta = 0; theta < 2*Math.PI; theta += Math.PI/100)
+			{
+				x = (c.x + (Math.sin(theta) * rayon));
+				y = (c.y + (Math.cos(theta) * rayon));
+			    gl.glVertex2d(x,y);
+			}
+		gl.glEnd();
 	}
 }
