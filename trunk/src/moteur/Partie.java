7633a6e999c0fb4a2a713fd10410293eaed96bd0
nivet1@hotmail.com <nivet1@hotmail.com@984e1f29-0a54-0ee0-b2e0-9a448cd44014>
2010-05-20 15:39:10 +0000

    - Ecoute des événements claviers - Chute automatique des pièces - Gestion de la file de pièces

--- trunk/src/moteur/Partie.java
+++ trunk/src/moteur/Partie.java
@@ -1,6 +1,45 @@
 package moteur;
 
+import java.util.concurrent.LinkedBlockingQueue;
+
 public class Partie
 {
+	private Plateau plateau;
+	private LinkedBlockingQueue<Piece> prochainesPieces;
+	private Piece pieceCourante;
+	private int score;
+	private int difficulte;
+	private int combo;
+	
+	public Partie()
+	{
+		plateau = new Plateau();
+		prochainesPieces = new LinkedBlockingQueue<Piece>(2);
+		score = 0;
+		difficulte = 0;
+		combo = 0;
+		
+		pieceCourante = new Piece();
+		plateau.ajouter(pieceCourante);
+		
+		prochainesPieces.add(new Piece());
+		prochainesPieces.add(new Piece());
+	}
+	
+	public Plateau getPlateau()
+	{
+		return plateau;
+	}
+	
+	public Piece getPieceCourante()
+	{
+		return pieceCourante;
+	}
 
+	public void chargerPieceSuivante()
+	{
+		pieceCourante = prochainesPieces.poll();
+		plateau.ajouter(pieceCourante);
+		prochainesPieces.add(new Piece());
+	}
 }
