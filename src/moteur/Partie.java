e620d404ca2bb969d1e865f1b8115b6765ac785f
remi.lacroix@gmail.com <remi.lacroix@gmail.com@984e1f29-0a54-0ee0-b2e0-9a448cd44014>
2010-05-30 17:16:17 +0000

    - Cosmétiques - Correction : le tableau de liens des puyos était trop grand - Correction : certaines méthodes n'avaient pas de raison d'être publiques - Ajout d'un minimum de javadoc ...

--- src/moteur/Partie.java
+++ src/moteur/Partie.java
@@ -2,16 +2,36 @@ package moteur;
 
 import java.util.concurrent.LinkedBlockingQueue;
 
+/**
+ * Classe représentant une partie, c'est-à-dire un plateau de jeu,
+ * un niveau de difficulté, un score, la pièce en train de chuter
+ * et les deux pièces à venir.
+ * @author Rémi Lacroix & Marie Nivet & Nicolas Poirier
+ *
+ */
 public class Partie
 {
+	/** Le plateau de jeu */
 	private Plateau plateau;
+	/** Une file contenant les deux prochaines pièces */
 	private LinkedBlockingQueue<Piece> prochainesPieces;
+	/** La pièce en cours de chute */
 	private Piece pieceCourante;
+	/** Le score courant */
 	private int score;
+	/** Le niveau de difficulté */
 	private int difficulte;
+	/** Le nombre d'enchaînements effectués */
 	private int combo;
-	private boolean start, pause;
+	/** Indique si la partie est commencée ou non */
+	private boolean start;
+	/** Indique si la partie est mise en pause ou non */
+	private boolean pause;
 	
+	/**
+	 * Crée une nouvelle partie non commencée avec un plateau vide
+	 * et deux pièces en attente.
+	 */
 	public Partie()
 	{
 		plateau = new Plateau();
@@ -26,16 +46,32 @@ public class Partie
 		prochainesPieces.add(new Piece());
 	}
 	
+	/**
+	 * Retourne le plateau correspondant à la partie.
+	 * @return le plateau de la partie.
+	 */
 	public Plateau getPlateau()
 	{
 		return plateau;
 	}
 	
+	/**
+	 * Retourne la pièce actuellement en cours de chute.
+	 * @return la pièce en chute.
+	 */
 	public Piece getPieceCourante()
 	{
 		return pieceCourante;
 	}
-
+	
+	/**
+	 * Prend la première pièce de la file d'attente pour l'utiliser
+	 * comme pièce courante et ajoute une nouvelle pièce en fin de
+	 * file pour maintenir le nombre de pièces en attente à 2.
+	 * Retourne un tableau contenant les deux pièces en attente
+	 * après le chargement de la pièce suivante.
+	 * @return un tableau contenant les deux pièces en attente.
+	 */
 	public Piece[] chargerPieceSuivante()
 	{
 		pieceCourante = prochainesPieces.poll();
@@ -45,31 +81,55 @@ public class Partie
 		return prochainesPieces.toArray(new Piece[2]);
 	}
 	
+	/**
+	 * Retourne les deux pièces actuellement en attente dans un
+	 * tableau.
+	 * @return un tableau contenant les deux pièces en attente.
+	 */
 	public Piece[] getPiecesSuivantes()
 	{
 		return prochainesPieces.toArray(new Piece[2]);
 	}
 	
+	/**
+	 * Permet d'indiquer que la partie est commencée.
+	 */
 	public void commencerPartie()
 	{
 		start = true;
 	}
 	
+	/**
+	 * Retourne vrai ou faux selon que la partie soit en cours ou non
+	 * @return un booléen indiquant si la partie est en cours.
+	 */
 	public boolean estEnCours()
 	{
 		return start;
 	}
 	
+	/**
+	 * Permet d'indiquer que la partie est actuellement mise en pause.
+	 */
 	public void mettreEnPause()
 	{
 		pause = true;
 	}
 	
+	/**
+	 * Permet d'enlever l'indication que la partie est actuellement
+	 * mise en pause.
+	 */
 	public void reprendrePartie()
 	{
 		pause = false;
 	}
 	
+	/**
+	 * Retourne vrai ou faux selon que la partie soit actuellement
+	 * mise en pause ou non.
+	 * @return un booléen indiquant si la partie est en pause.
+	 */
 	public boolean estEnPause()
 	{
 		return pause;
--- src/moteur/Puyo.java
+++ src/moteur/Puyo.java
@@ -3,8 +3,21 @@ package moteur;
 import java.awt.Color;
 import java.util.Random;
 
+/**
+ * Classe représentant un puyo. Le puyo possède juste une couleur et sait
+ * s'il possède un lien avec le puyo juste au dessus de lui ou le puyo
+ * juste à sa droite. L'égalité entre deux puyos peut être testée : s'ils
+ * ont la même couleur, ils sont considérés comme égaux.
+ * A sa création le puyo peut prend une couleur aléatoire parmi un choix de quatre,
+ * si sa couleur n'est pas indiqué.
+ * @author Rémi Lacroix & Marie Nivet & Nicolas Poirier
+ *
+ */
 public class Puyo
 {
+	/** Générateur de nombres aléatoires partagé par tous les puyos */
+	private static Random rand = new Random();
+	/** Couleurs possibles pour le puyo, utilisées pour le choix aléatoire d'une couleur */
 	private static final Color[] couleurs = 
 	{
 		Color.MAGENTA,
@@ -12,40 +25,72 @@ public class Puyo
 		Color.GREEN,
 		Color.BLACK
 	};
+	/** La couleur du puyo */
 	private Color couleur;
-	private static Random rand = new Random();
+	/** Constante définissant l'indice du lien vers la droite */
 	public static final int DROITE = 0;
+	/** Constante définissant l'indice du lien vers le haut */
 	public static final int HAUT = 1;
+	/** Tableaux permettant de stocker la présence des liens */
 	private boolean[] liens;
 	
+	/**
+	 * Crée un nouveau puyo avec une couleur aléatoire choisie parmi les
+	 * quatre possibles et aucun lien avec ces voisins.
+	 */ 
 	public Puyo()
 	{
 		this(couleurs[rand.nextInt(couleurs.length)]);
 	}
 	
-	public Puyo(Color couleur)
+	/**
+	 * Crée un nouveau possédant la couleur couleur et aucun lien avec ces voisins.
+	 * @param couleur la couleur du puyo
+	 */
+	private Puyo(Color couleur)
 	{
 		this.couleur = couleur;
 		
-		liens = new boolean[4];
+		liens = new boolean[2];
 		liens[DROITE] = liens[HAUT] = false;
 	}
 	
+	/**
+	 * Retourne la couleur du puyo considéré.
+	 * @return la couleur du puyo.
+	 */
 	public Color getCouleur()
 	{
 		return couleur;
 	}
 	
+	/**
+	 * Retourne vrai ou faux selon que le lien désigné par l'indice lien
+	 * existe ou non.
+	 * @param lien l'indice correspondant au lien voulu.
+	 * @return un booleen indiquant l'existance du lien.
+	 */
 	public boolean getLien(int lien)
 	{
 		return liens[lien];
 	}
 	
+	/**
+	 * Permet d'indiquer que le lien désigné par l'indice lien existe.
+	 * @param lien l'indice correspondant au lien.
+	 */
 	public void setLien(int lien)
 	{
 		liens[lien] = true;
 	}
 	
+	/**
+	 * Retourne vrai ou faux selon que le puyo soit égal à l'objet testé.
+	 * Le résultat est vrai si l'objet testé est un puyo de couleur identique
+	 * à ce puyo.
+	 * @param obj l'objet à tester.
+	 * @return un booleen indiquant l'égalité entre le puyo et l'objet.
+	 */
 	public boolean equals(Object obj)
 	{
 		boolean egaux = false;
