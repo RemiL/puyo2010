28f938080427d1a4f5c07924c3ffb58be375b411
remi.lacroix@gmail.com <remi.lacroix@gmail.com@984e1f29-0a54-0ee0-b2e0-9a448cd44014>
2010-06-13 17:06:29 +0000

    - Ajout de la sauvegarde des meilleurs scores - Ajout de la visualisation des meilleurs scores (touche F2, m ou h)

--- src/graphique/FenetrePrincipale.java
+++ src/graphique/FenetrePrincipale.java
@@ -2,8 +2,14 @@ package graphique;
 
 import java.awt.BorderLayout;
 import java.awt.Container;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+import moteur.MeilleursScores;
 
 /**
  * Classe dérivant d'une JFrame et représentant la fenêtre principale de l'application.
@@ -28,5 +34,35 @@ public class FenetrePrincipale extends JFrame
 		this.setVisible(true);
 		
 		zdj.requestFocus();
+	}
+
+	public String demandeNom()
+	{
+		String nom;
+		
+		do {
+			nom = JOptionPane.showInputDialog(this, "Félicitation, vous venez de faire un nouveau meilleur score !\nEntrez votre nom :", "Nouveau meilleur score", JOptionPane.QUESTION_MESSAGE);
+		} while (nom == null);
+		
+		return nom;
+	}
+	
+	public void afficheMeilleuresScores(MeilleursScores meilleursScores)
+	{
+		StringBuilder msg = new StringBuilder();
+		int i = meilleursScores.size();
+		
+		for (Entry<Integer, LinkedList<String>> entree : meilleursScores.entrySet())
+		{
+			for (String nom : entree.getValue())
+			{
+				msg.insert(0, i+") "+nom+" : "+entree.getKey()+"\n");
+				i--;
+			}
+		}
+		
+		msg.insert(0, "Meilleurs scores :\n\n");
+		
+		JOptionPane.showMessageDialog(this, msg.toString(), "Meilleurs scores", JOptionPane.INFORMATION_MESSAGE);
 	}
 }
